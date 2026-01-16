76c09a9d9a2d0ca2ea9b205734048cb4870912cd
Sourav Dhar <sourav.d@terrapay.com>
2026-01-16 16:38:31 +0530

    added config and security scripts

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Settings:
+    PROJECT_NAME = 'car price prediction'
+    API_KEY = os.getenv('API_KEY', 'demo-key')
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", 'secret')
+    JWT_ALGORITHM = 'HS256'
+    REDIS_URL = os.getenv('REDIS_URL', 'redis:///localhost:6379')
+    MODEL_PATH = 'app/models/model.joblib'
+
+settings = Settings()

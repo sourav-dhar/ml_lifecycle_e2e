0a4fd7c65607b4f4b60bb7fef717ac08a8649848
Sourav Dhar <sourav.d@terrapay.com>
2026-01-17 11:35:40 +0530

    added files till routes_predict

---
+++ app/api/routes_predict.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from app.core.dependencies import get_api_key, get_current_user
+from app.services.model_service import predict_car_price
+
+
+
+router = APIRouter()
+
+class CarFeatures(BaseModel):
+    company: str
+    year: int
+    fuel: str
+    seller_type: str
+    transmission: str
+    km_driven: float
+    mileage_mpg: float
+    engine_cc: float
+    max_pwer_bhp: float
+    torque_nm: float
+    seats: float
+
+
+@router.post("/predict")
+def predict_price(car: CarFeatures, user =Depends(get_current_user), _ = Depends(get_api_key)):
+    prediction = predict_car_price(car.model_dump())
+    return ({'predicted_price': prediction})
+
---
+++ training/train_models.py
@@ -0,0 +1,60 @@
+import os
+import pandas as pd
+import joblib
+from training.train_utils import DATA_FILE_PATH, MODEL_DIR, MODEL_PATH
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+
+data = (pd
+        .read_csv(DATA_FILE_PATH)
+        .drop_duplicates()
+        .drop(columns= ['name', 'model', 'edition']))
+
+
+X = data.drop(columns = ['selling_price'])
+y = data['selling_price']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)
+
+num_cols = X_train.select_dtypes(include='number').columns.tolist()
+cat_cols = [col for col in X_train.columns if col not in num_cols]
+
+num_pipe = Pipeline(
+    steps = [
+        ('imputer', SimpleImputer(strategy = 'median')),
+        ('scaler', StandardScaler()),
+    ]
+)
+
+cat_pipe = Pipeline(
+    steps = [
+        ('imputer', SimpleImputer(strategy = 'constant', fill_value= 'missing')),
+        ('encoder', OneHotEncoder(handle_unknown= 'ignore',sparse_output=False))
+    ]
+)
+preprocessor = ColumnTransformer(transformers=[
+    ('num', num_pipe, num_cols),
+    ('cat', cat_pipe, cat_cols)
+])
+
+regressor = RandomForestRegressor(
+    n_estimators=10, max_depth = 5, random_state = 42
+)
+
+rf_model = Pipeline(
+    steps=[
+        ('pre', preprocessor),
+        ('reg', regressor)
+    ]
+)
+
+rf_model.fit(X_train, y_train)
+
+os.makedirs(MODEL_DIR, exist_ok=True)
+
+joblib.dump(rf_model, MODEL_PATH)
+print("The model has been dumped successfully")
